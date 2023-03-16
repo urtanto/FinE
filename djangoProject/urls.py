b9d6751c318564a669a2c86193a4ba3ea5b271d3
Anikanov Vlad <anikanovvlad@vk.com>
2023-03-16 21:45:30 +0500

    based django

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from fine import views
+
 urlpatterns = [
+    path('', views.index_page, name='index'),
     path('admin/', admin.site.urls),
 ]
