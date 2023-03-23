d196506ae17ea208573664e76f623338f4c5bc57
Nikita Kivalin <KIvalinNik@mail.ru>
2023-03-23 18:29:53 +0300

    Add profile

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from djangoProject import settings
 from fine import views
 
 urlpatterns = [
     path('', views.index_page, name='index'),
     path('admin/', admin.site.urls),
+    path('profile/<int:code>', views.profile_view_page, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- fine/views.py
+++ fine/views.py
@@ -1,6 +1,9 @@
 # pylint: disable=C0114
 from django.core.handlers.wsgi import WSGIRequest
+from django.http import Http404
 from django.shortcuts import render
+
+from fine.models import RegistrationEvents, User, Interests
 
 
 def get_menu_context():
@@ -24,3 +27,29 @@ def index_page(request: WSGIRequest):
         'menu': get_menu_context()
     }
     return render(request, 'pages/index.html', context)
+
+def cheack_for_none(id, model):
+    try:
+        temp = model.objects.get(user=id)
+
+        return temp
+    except model.DoesNotExist:
+        return None
+
+
+def profile_view_page(request: WSGIRequest, code: int):
+    """
+    Профиль пользователя
+    """
+    context = {'pagename': 'Profile',
+               'menu': get_menu_context() }
+    try:
+        context['user'] = User.objects.get(id=code) # все поля из модели для пользователя с id = code
+        context['events'] = cheack_for_none(code, RegistrationEvents) # ивенты, на которые зарегался пользователь
+        context['interests'] = cheack_for_none(code, Interests) # интересы пользователя
+    except User.DoesNotExist:
+        context['events'] = None
+        context['interests'] = None
+        raise Http404
+
+    return render(request, 'pages/profile/view.html', context)
