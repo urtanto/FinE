e48cebe7bf58894ad52a769fff503c8526043fa7
Anikanov Vlad <anikanovvlad@vk.com>
2023-05-21 16:15:11 +0500

    fix docker-compose

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python3 manage.py migrate
 python3 manage.py collectstatic
-gunicorn --workers 3 -b 0.0.0.0:8000 fine_project.wsgi:application --access-logfile /proj/gunicorn_access.log --error-logfile /proj/gunicorn_error.log
+gunicorn fine_project.wsgi:application -b 0.0.0.0:8000 --timeout 120 --workers 3
