a8ae02a7689ab8867a68773f9b4b6712691baab0
Wyndace <wyndace.av@ya.ru>
2023-05-21 12:12:41 +0000

    feat: добавлено переключение табов через js

--- fine/static/js/tabs.js
+++ fine/static/js/tabs.js
@@ -2,14 +2,23 @@ const tabsContainer = document.querySelectorAll(".tabs");
 if (tabsContainer.length) {
   tabsContainer.forEach((container) => {
     const buttons = container.querySelectorAll("[name=tab-btn]");
-    const contents = container.querySelectorAll("[id~=content-]");
-    console.log(contents);
+    const contents = container.querySelectorAll("[id*=content-]");
     if (buttons) {
       buttons.forEach((btn) => {
+        const containerNumber = Number(
+          btn.getAttribute("id").replace("tab-btn-", "")
+        );
+        if (btn.checked) {
+          contents[containerNumber - 1].classList.add("active");
+        }
         btn.addEventListener("click", () => {
-          contents.forEach((content) => content.classList.remove("active"));
           const containerNumber = Number(
             btn.getAttribute("id").replace("tab-btn-", "")
+          );
+          contents.forEach((content) =>
+            content.classList.contains("active")
+              ? content.classList.remove("active")
+              : 0
           );
           contents[containerNumber - 1].classList.add("active");
         });
