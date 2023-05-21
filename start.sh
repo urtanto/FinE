6e03f3bf1fb30cde6868772f385198349305cc97
Wyndace <wyndace.av@ya.ru>
2023-05-21 10:58:15 +0000

    cringe

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python3 manage.py migrate
 python3 manage.py collectstatic
-gunicorn fine_project.wsgi:application -b 0.0.0.0:8000 --timeout 120
+gunicorn fine_project.wsgi:application --workers 3 -b 0.0.0.0:8000 --access-logfile ~/FinE/gunicorn_access.log --error-logfile ~/FinE/gunicorn_error.log 
